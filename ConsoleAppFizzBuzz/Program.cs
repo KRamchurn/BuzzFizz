ffeab2eab77ee7478690c3bbcb5c88e5f764415e
KRamchurn <36997679+KRamchurn@users.noreply.github.com>
2019-08-13 15:21:30 +0100

    Add files via upload

---
+++ ConsoleAppFizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFizzBuzz
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 1;
+			while (a < 100)
+			{
+
+				if ((a % 15) == 0)
+					Console.WriteLine("FizzBuzz");
+				else if ((a % 3) == 0)
+					Console.WriteLine("Fizz");
+				else if ((a % 5) == 0)
+					Console.WriteLine("Buzz");
+				else
+					Console.WriteLine(a);
+				a++;
+				
+			}
+
+
+		}
+	}
+}
